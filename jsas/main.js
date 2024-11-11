7a6d3272584d2987855fd111eb7bef28a28c0c34
TMG <alex800kill@gmail.com>
2024-11-11 13:14:14 +0200

    Bull

---
+++ jsas/main.js
@@ -0,0 +1,11 @@
+function gosho(){
+    document.getElementById('gosho').innerHTML = "Chist Gosho!";
+}
+
+function load(){
+    alert("Това е съобщение от JS!");
+}
+
+function msg(){
+    alert("Отиди навън и пипни трева!");
+}
