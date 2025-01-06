69d24bfaee884acb37d61e5525972a0db156aace
TMG <alex800kill@gmail.com>
2025-01-06 12:56:09 +0200

    Doom

---
+++ shgdgjh/main.js
@@ -0,0 +1,14 @@
+
+
+function start(){
+    document.getElementById("main").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
+
+function second(){
+    const text = document.getElementById("text")
+    text.style.textAlign = "end";
+    text.style.fontSize = "100px";
+    text.style.fontFamily = "Playwrite IE Guides", serif;
+    text.style.textShadow = "2px 2px 5px gray";
+}
